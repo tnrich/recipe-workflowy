e8bce515f71a76555015057ea109a6c9745c977d
tnrich <tnrich@teselagen.com>
2018-11-05 14:50:15 -0800

    workflowy recipe v0.1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# recipe-workflowy
-franz workflowy recipe
+# Workflowy for Franz
+This is the official Franz recipe for [Workflowy](http://workflowy.com)
+
+### How to create your own Franz recipes:
+* [Read the documentation](https://github.com/meetfranz/plugins)
---
+++ webview.js
@@ -0,0 +1,10 @@
+module.exports = (Franz) => {
+  // const getMessages = function getMessages() {
+  //   const directMessages = document.querySelectorAll('.badge-count:not(.ng-hide)').length;
+
+  //   // set Franz badge
+  //   Franz.setBadge(directMessages);
+  // };
+
+  // Franz.loop(getMessages);
+};
